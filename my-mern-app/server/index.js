2445cfb50bcfc6d394eee073bc1e13a33549cad9
Irene Pavlenko <irenpavlenko22@gmail.com>
2024-10-01 21:11:54 -0400

    more fixes for vercel deploy

--- my-mern-app/server/index.js
+++ my-mern-app/server/index.js
@@ -1,7 +1,6 @@
-"use strict";
-
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const { createUser } = require("./handlers/createUser");
 const { loginUser } = require("./handlers/loginUser");
@@ -10,37 +9,39 @@ const { deleteUser } = require("./handlers/deleteUser");
 const {
   getWeatherRecommendation
 } = require("./handlers/getWeatherRecommendation");
-const cors = require("cors");
 
 const PORT = process.env.PORT || 5001;
 
 const app = express();
-app.use(cors());
-app.use(express.json());
 
-app
-  .use(function(req, res, next) {
-    res.header(
-      "Access-Control-Allow-Methods",
-      "OPTIONS, HEAD, GET, PUT, POST, DELETE"
-    );
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
+// Enable CORS for frontend to connect to the backend
+app.use(
+  cors({
+    origin: "*", // You can restrict this to your frontend URL for more security
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Access-Control-Allow-Headers"
+    ]
   })
-  .use(morgan("tiny"))
-  .use(express.static("./server/assets"))
-  .use(express.json())
-  .use(express.urlencoded({ extended: false }))
-  .use("/", express.static(__dirname + "/"));
+);
 
-// User Routes
+app.use(express.json());
+app.use(morgan("tiny"));
+
+// Serve static files from the assets directory
+app.use(express.static("./server/assets"));
+
+// API Endpoints
 app.post("/api/signup", createUser);
 app.post("/api/login", loginUser);
 app.put("/update/:id", updateUser);
 app.delete("/delete", deleteUser);
 app.get("/api/recommendation/:weather_condition", getWeatherRecommendation);
 
-app.listen(PORT, () => console.info(`Listening on port ${PORT}`));
+// Catch-all route for serving the backend root URL
+app.use("/", express.static(__dirname + "/"));
+
+// Start the server
+app.listen(PORT, () => console.info(`Server running on port ${PORT}`));
